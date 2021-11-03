6c4581923f82f52d88a6888adc9d764430b28580
Everton Ulisses de Mendonça <evertonulisystem@gmail.com>
2021-11-02 21:57:43 -0300

    Projeto Entendimento de Teste Unitário

---
+++ src/main/java/br/gotasdetecnologia/calculadora/Calculos.java
@@ -0,0 +1,20 @@
+package br.gotasdetecnologia.calculadora;
+public class Calculos {
+    public double soma(double... numeros){
+        double soma=0;
+           if (numeros.length>0){
+                for(double numeroASomar : numeros){
+                soma += numeroASomar;
+                }
+            }
+        return soma;
+    }
+    public boolean informaNumeroPar (int numero) {
+        if (numero % 2 ==0) {
+            return true;
+        } return false;
+    }
+    public double raizQuadradaDeDoisNumeros(double numero){
+          return Math.sqrt(numero);
+        }
+}
---
+++ src/test/java/br/gotasdetecnologia/calculadora/CalculosTest.java
@@ -0,0 +1,41 @@
+package br.gotasdetecnologia.calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import org.junit.Assert;
+//import org.junit.Test;
+public class CalculosTest {
+    //CENÁRIO DE TESTE
+    //Preparação do Cenário
+    //Execução
+    //Resultados/Verificações
+    @Test
+    public void retornarZeroQuandoNadaInformado(){
+        Calculos numerosASomar = new Calculos();
+        double esperado = 0;
+        double atual = numerosASomar.soma();
+        Assertions.assertEquals(esperado,atual);
+        //Assert.assertEquals(esperado,atual,0);
+  }
+    @Test
+    public void retornarSomaQuandoValoresInformados(){
+        Calculos numerosASomar = new Calculos();
+        double atual = numerosASomar.soma(150,450);
+        double esperado = 600;
+        Assertions.assertEquals(esperado, atual);
+        //Assert.assertEquals(esperado,atual,0);
+  }
+  @Test
+  public void verificaNumeroPar() {
+      Calculos paraVerificar = new Calculos();
+      boolean informaNumeroPar = paraVerificar.informaNumeroPar(4);
+      Assertions.assertTrue(informaNumeroPar);
+  }
+
+  @Test
+    public void retornaRaizQuadrada(){
+      Calculos numeroACalcular = new Calculos();
+      double atual = numeroACalcular.raizQuadradaDeDoisNumeros(144);
+      double esperado = 12;
+      Assertions.assertEquals(esperado, atual);
+    }
+}
